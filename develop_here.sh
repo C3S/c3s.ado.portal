f2b4891f93ddb396d59e526b41b67c1cf7a6746d
Christoph Scheid <c@shri.de>
2014-09-29 09:03:24 +0200

    scaffolding --> development

--- develop_here.sh
+++ develop_here.sh
@@ -12,11 +12,11 @@ virtualenv env
 
 # create template project from scaffold
 # (this is a one off: you need this only once! created files are versioned)
-./env/bin/pcreate -t cornice -t alchemy ../c3s.ado.portal
+#./env/bin/pcreate -t cornice -t alchemy ../c3s.ado.portal
 
 # set it up for development; pulls in the dependencies
-#./env/bin/python setup.py develop
+./env/bin/python setup.py develop
 
 # start the app
 # * the reload option will restart the app every time a source file changes
-#./env/bin/pserve development.ini --reload
+./env/bin/pserve development.ini --reload
